d5275fdb24805d723a4cfe76ddca523db4a5ffc9
SuperHands <anthon3869@gmail.com>
2025-11-01 14:03:35 -0500

    Guard against missing ReactReduxContext

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -2,4 +2,15 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-createRoot(document.getElementById('root')!).render(<App/>)
+
+declare global {
+  interface Window {
+    ReactReduxContext?: React.Context<unknown>
+  }
+}
+
+if (typeof window !== 'undefined' && !window.ReactReduxContext) {
+  window.ReactReduxContext = React.createContext(undefined)
+}
+
+createRoot(document.getElementById('root')!).render(<App />)
